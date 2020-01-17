c2022a84455e3887a4d88fde815ae7b13ba48ebe
Tristan <kraig.tri125@gmail.com>
2020-01-17 18:56:07 -0500

    Init

---
+++ KTApp.Business/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using KTApp.Models;
+
+namespace KTApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
